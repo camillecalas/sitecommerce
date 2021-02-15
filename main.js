e876538c45664bd56ca02b1dd0cdaa15be33de60
camille <camillecalas@hotmail.fr>
2021-02-15 10:45:32 +0100

    page vetement demarrage

--- main.js
+++ main.js
@@ -18,7 +18,8 @@ tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
 tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
 tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
 tl.fromTo(".icones", { opacity: 0 }, { opacity: 1, duration: 1 });
-tl.fromTo(".big-text", { opacity: 0 }, { opacity: 1, duration: 0.2 });
+tl.fromTo(".big-text", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
+
 
 
 
