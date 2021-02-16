1793b909010a11b3cb825a711cd716847dfedfd4
camille <camillecalas@hotmail.fr>
2021-02-16 13:40:49 +0100

    changes

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 
+// MENU BURGER
+
 const burger = document.querySelector('.burger');
 const menu = document.querySelector('nav');
 const menuDeskstop = document.querySelector('.pretAPorter');
@@ -11,6 +13,9 @@ menu.classList.toggle('menu-open');
 // menu.classList.toggle('menu-open');
 // })
 
+
+
+// INTRO ANIMATION
 const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
 tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
@@ -19,6 +24,29 @@ tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
 tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1});
 tl.fromTo(".icones", { opacity: 0 }, { opacity: 1, duration: 1}, "-=1");
 tl.fromTo(".big-text", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
+
+
+
+
+
+
+// SCROLL HEADER
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("myHeader");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
 
 
 
--- vetement.js
+++ vetement.js
@@ -1,3 +1,26 @@
+// SCROLL HEADER
+
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("myHeader");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+
+
+
+// CARROUSSEL 
 let thumbnails = document.getElementsByClassName('thumbnail')
 
 let activeImages = document.getElementsByClassName('active')
@@ -10,21 +33,24 @@ for (var i=0; i < thumbnails.length; i++){
         if (activeImages.length > 0){
             activeImages[0].classList.remove('active')
         }
-        
-
+  
         this.classList.add('active')
         document.getElementById('featured').src = this.src
     })
 }
 
 
-let buttonRight = document.getElementById('slideRight');
-let buttonLeft = document.getElementById('slideLeft');
+// let buttonRight = document.getElementById('slideRight');
+// let buttonLeft = document.getElementById('slideLeft');
 
-buttonLeft.addEventListener('click', function(){
-    document.getElementById('slider').scrollLeft -= 180
-})
+// buttonLeft.addEventListener('click', function(){
+//     document.getElementById('slider').scrollLeft -= 180
+// })
 
-buttonRight.addEventListener('click', function(){
-    document.getElementById('slider').scrollLeft += 180
-})
+// buttonRight.addEventListener('click', function(){
+//     document.getElementById('slider').scrollLeft += 180
+// })
+
+
+
+
